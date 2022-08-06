f63eb4458c74b641ca2df307ab5bd7d5d54c7bdb
Thiago Alves <thiago.vmatos@hotmail.com>
2022-08-06 19:31:45 -0300

    Implementação do padrão estrutural: Facade.

---
+++ src/subsistema2/cep/CepApi.java
@@ -0,0 +1,21 @@
+package subsistema2.cep;
+
+public class CepApi {
+    private static CepApi instance = new CepApi();
+
+    private CepApi(){
+        super();
+    }
+
+    public static CepApi getInstance(){
+        return instance;
+    }
+
+    public String recuperarCidade(String cep){
+        return "Araraquara";
+    }
+
+    public String recuperarEstado(String cep){
+        return "SP";
+    }
+}
